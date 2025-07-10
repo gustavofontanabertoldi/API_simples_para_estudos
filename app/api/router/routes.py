5238dbe3894f57ba38b5fe01131ec632542d0283
Gustavo Fontana Bertoldi <gustavobertoldi20@gmail.com>
2025-07-10 08:30:47 -0300

    Deleting the user for a moment

--- app/api/router/routes.py
+++ app/api/router/routes.py
@@ -2,12 +2,10 @@ from fastapi import APIRouter, Depends
 from fastapi.exceptions import HTTPException
 from app.db.connection import get_db
 from sqlalchemy.ext.asyncio import AsyncSession
-from app.schemas.DTOs.userDTO import UserDTO
 from app.schemas.DTOs.productDTO import ProductDTO
 from app.services.product import get_product_by_name, delete_product_by_name
 from app.services.product import create_product as create_product_service
 from fastapi.security import OAuth2PasswordBearer
-from app.services.user import get_current_user, register_user
 
 router = APIRouter()
 
@@ -29,10 +27,3 @@ async def create_product(new_product:ProductDTO, db:AsyncSession = Depends(get_d
 async def delete_product(name:str, db:AsyncSession = Depends(get_db)):
     await delete_product_by_name(db=db, name=name)
     return {"message": f"Produto '{name}' deletado com sucesso"}
-
-#-------------------USER-----------------------
-
-@router.post("/register")
-async def add_user(new_user:UserDTO, db:AsyncSession=Depends(get_db)):
-    created_user = await register_user(db=db, new_user=new_user)
-    return created_user
